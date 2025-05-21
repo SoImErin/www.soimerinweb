cb26e96c1e4f6978495003650c72c7e171409fb6
Erin Sophia Violet Boundy <erinsvb.1311@gmail.com>
2025-05-21 02:42:17 +0100

    e

--- code/AnimateLandingMobile.js
+++ code/AnimateLandingMobile.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const contentRoot = document.querySelector('.contentroot');
-  const headingContainer = document.querySelector('.heading__container');
+  const contentRoot = document.querySelector('.body__container');
 
   function checkScrollTop() {
     const atTop = window.scrollY === 0;
 
     if (atTop) {
       contentRoot?.classList.add('scroll-at-top');
-      headingContainer?.classList.add('scroll-at-top');
     } else {
       contentRoot?.classList.remove('scroll-at-top');
-      headingContainer?.classList.remove('scroll-at-top');
     }
   }
 
